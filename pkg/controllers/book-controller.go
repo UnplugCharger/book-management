8ee380bd522b154f2d31dc1933a7e9ef6889a06b
UnplugCharger <isaacbyron@gmail.com>
2022-03-07 19:26:41 +0300

    controllers wp

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,2 +1,49 @@
 package controllers
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/UnplugCharger/book-management/pkg/models"
+	"github.com/UnplugCharger/book-management/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+
+
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request)  {
+	newBook := models.GetAllBooks()
+	res , _ := json.Marshal(newBook)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request)  {
+	vars := mux.Vars(r)
+	bookId:= vars["bookId"]
+	ID, err := strconv.ParseInt(bookId,0,0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+
+	}
+	bookDetails,_:=models.GetBookById(ID)
+	res ,_ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+func CreateBook(w http.ResponseWriter, r *http.Request)  {
+	CreateBook:= &models.Book{}
+	utils.ParseBody(r ,CreateBook)
+	b:= CreateBook.CreateBook()
+	res ,_ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
